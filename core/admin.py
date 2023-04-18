066cd377a7c9fa2aae3f116d95b3a0c59ede03d6
Marcos Levi <levi13.marcos@gmail.com>
2023-04-18 19:05:13 -0300

    c1 ajustando secretkey

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(Categoria)
+admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=255, verbose_name='Descrição')
+
+    def __str__(self) -> str:
+        return f'{self.descricao}'
+
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=255, verbose_name='Nome')
+    site = models.URLField(max_length=255, verbose_name='Site')
+
+    def __str__(self) -> str:
+        return f'{self.nome}'
+
+
+class Autor(models.Model):
+    class Meta:
+        verbose_name_plural = 'Autores'
+
+    nome = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f'{self.nome}'
+
+
+class Livro(models.Model):
+    class Meta:
+        verbose_name_plural = 'Livros'
+
+    titulo = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=255)
+    quantidade = models.IntegerField()
+    preco = models.FloatField()
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name='livros')
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name='livros')
+    autores = models.ManyToManyField(Autor, related_name='livros')
+
+    def __str__(self) -> str:
+        return f'{self.titulo}/{self.editora}'
